29de184137d39564641296d0d92195fa165215c7
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2010-11-15 16:58:45 +0000

    Inicio projeto HCI Prototipo

---
+++ 2-Disciplinas/HCI/Projeto/P3/Prototipo/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Projeto Faça a Festa - Página Inicial</title>
+<link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+
+<body>
+<!-- Colocar o header em um include -->
+<div id="header">
+	<div id="logo">Logo Projeto</div>
+    <div id="login">
+    	<a href="#" title="Cadastrar">Cadastrar</a>
+        &nbsp;|&nbsp;
+    	<a href="#" title="Fazer Login">Fazer Login</a>
+        <br />
+        <div class="left">
+        	<a href="#" title="Criar Festa">Criar Festa</a>
+            <br />
+            <a href="#" title="Divulgar Festa">Divulgar Festa</a>
+            <br />
+            <a href="#" title="Gerenciar">Gerenciar</a>
+            <br />
+            <a href="#" title="Sair">Sair</a>
+        </div>
+        <div class="right">
+        	<a href="#" title="Minha Conta">Minha Conta</a>
+            <br />
+            <a href="#" title="Meus Favoritos">Meus Favoritos</a>
+            <br />
+            <a href="#" title="Minhas Festas">Minhas Festas</a>
+            <br />
+            <a href="#" title="Meus Convidados">Meus Convidados</a>
+            <br />
+            <a href="#" title="Meus Contatos">Meus Contatos</a>
+        </div>
+    </div>
+</div>
+<div id="container">
+	<!-- Colocar o left-bar em um include -->
+    <div id="left-bar">
+    	<h2>Pesquisar Fornecedores</h2>
+        <p>Pesquisa por palavra chave:</p>
+        <input type="text" />
+        <input type="submit" value="Pesquisar" />
+        <hr />
+        <p>Pesquisa por categoria:</p>
+        <select>
+        	<option value="Locais">Locais</option>
+            <option value="Bandas">Bandas</option>
+            <option value="Buffet">Buffet</option>
+            <option value="Decoração">Decoração</option>
+            <option value="Bolo & Doces">Bolo &amp; Doces</option>
+            <option value="Bebidas">Bebidas</option>
+            <option value="Artigos para Festas">Artigos para Festas</option>
+            <option value="Lembranças &amp; Brindes">Lembranças &amp; Brindes</option>
+            <option value="Cozinheira">Cozinheira</option>
+            <option value="Churrasqueiro">Churrasqueiro</option>
+            <option value="Açougue">Açougue</option>
+            <option value="Serviço de Limpeza">Serviço de Limpeza</option>
+            <option value="Animadores de Festa">Animadores de Festa</option>
+            <option value="DJ">DJ</option>
+            <option value="Seguranças">Seguranças</option>        
+        </select>
+		<input type="submit" value="Pesquisar" />
+    </div>
+    <div id="content">Área de conteúdo</div>
+</div>
+</body>
+</html>
