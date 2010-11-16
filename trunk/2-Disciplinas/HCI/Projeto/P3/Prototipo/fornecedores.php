299b05f1b62a7775b9660edb84276bdfedfe5433
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2010-11-16 00:17:47 +0000

    Adicao de arquivos ao projeto faca a festa imagens pagina de busca header.php left-bar.php modificação do arquivo index.php e css/main.css

---
+++ trunk/2-Disciplinas/HCI/Projeto/P3/Prototipo/fornecedores.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Projeto Faça a Festa - Busca de fornecedores</title>
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.2.0/build/cssreset/reset-min.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+
+<body>
+<?php include 'header.php'; ?>
+<div id="container">
+	<?php include 'left-bar.php'; ?>
+    <div id="content">
+    <div id="content-list" class="left">
+    	<p>Resultados da busca para: <strong>Locais</strong></p>
+        <ol>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        <li class="clear">
+        	<img width="70" height="70" src="img/bob-esponja.jpg" class="left" />
+			<p class="small right"><a href="#" title="mais detalhes">mais detalhes</a></p>
+            <h3>Fornecedor 1</h3>
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />
+            <img width="15" height="15" src="img/star.gif" />            
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+        </ol>
+	</div>
+    <div id="content-details" class="right">
+   	    <div id="content-empty">Clique em um dos fornecedores ao lado e veja aqui mais detalhes sobre ele.</div>
+    	<h1>Fornecedor 1</h1>
+        <img width="15" height="15" src="img/star.gif" />
+        <img width="15" height="15" src="img/star.gif" />
+        <img width="15" height="15" src="img/star.gif" />
+        <img width="15" height="15" src="img/star.gif" />
+        <img width="15" height="15" src="img/star.gif" />
+        <a class="small" href="#" title="Adicionar a favoritos">Adicionar a favoritos</a>
+        <a class="small" href="#" title="Sinalizar como spam">Sinalizar como spam</a>
+        
+        <div class="error-message"><a href="#" title="Cadastrar">Cadastre-se</a> ou <a href="#" title="Fazer Login">faça login</a> para realizar esta ação!</div>
+        
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum, 235 - Lorem Ipsum/SP</p>
+        <p>(xx) xxxx - xxxx</p>
+        <p><a href="mailto:lorem@ipsum.com.br" title="Email: lorem@ipsum.com.br">lorem@ipsum.com.br</a></p>
+        <p><a href="#" title="Acessar o site do fornecedor">http://www.lipsum.com.br</a></p>
+        
+        <hr />
+        
+        <h2>Fotos <span class="small">(clique na foto para expandi-la)</span></h2>
+        <div id=photo-gallery></div>
+        
+        <hr />
+        
+        <h2>Comentários <span class="small">(<a href="#" title="Deixe seu comentário">comente</a>)</span></h2>
+        <div class="error-message"><a href="#" title="Cadastrar">Cadastre-se</a> ou <a href="#" title="Fazer Login">faça login</a> para realizar esta ação!</div>
+        <div class="top-comment"><a href="#" title="Veja o perfil do usuário">Usuário A</a><span class="small"> (1 dia atrás): </span> Consectetur adipiscing elit. Nulla id neque ipsum, eu porta purus. Vivamus id tellus ac ipsum elementum consectetur. Sed viverra mi enim, vitae vestibulum libero.</div>
+        <div class="inner-comment"><a href="#" title="Veja o perfil do usuário">Usuário A</a><span class="small"> (1 dia atrás): </span> Curabitur vel orci nunc. Praesent eget massa purus, sed tempor nulla. Integer at sapien non metus lacinia dapibus nec non eros.</div>
+        <div class="inner-comment"><a href="#" title="Veja o perfil do usuário">Usuário A</a><span class="small"> (1 dia atrás): </span> uis pharetra enim a magna gravida sollicitudin. Phasellus semper eros in augue tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut diam ac metus varius scelerisque a in dui.</div>
+        <div class="inner-comment"><a href="#" title="Veja o perfil do usuário">Usuário A</a><span class="small"> (1 dia atrás): </span> Vestibulum eget urna vitae felis sodales congue a ut metus. Nulla interdum commodo metus quis interdum. Maecenas porttitor consectetur arcu, eu mollis nulla tincidunt at. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
+        <div class="bottom-comment"><a href="#" title="Veja o perfil do usuário">Usuário A</a><span class="small"> (1 dia atrás): </span> Sed sit amet velit eget eros lobortis sagittis vitae sed metus. Quisque vitae nunc nibh. Mauris vitae mauris arcu, non porttitor velit. Aliquam dictum justo ut enim volutpat eget dictum diam consectetur. Fusce lorem odio, accumsan ut lobortis ac, vulputate ac lectus. </div>
+    </div><!--#content-details-->
+    </div><!--#content-->
+</div>
+</body>
+</html>
