c5f284f78fb609a62d96b74efdc8715130ead2b4
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2010-11-17 00:03:03 +0000

    Criação de arquivos de javascript para interação Tratamento de imagens Adição de texto

--- 2-Disciplinas/HCI/Projeto/P3/Prototipo/left-bar.php
+++ 2-Disciplinas/HCI/Projeto/P3/Prototipo/left-bar.php
@@ -1,28 +1,37 @@
 <!-- Barra de Pesquisa -->
 <div id="left-bar">
-    <h2>Pesquisar Fornecedores</h2>
-    <p class="small">Pesquisando resultados para: <strong>São Carlos/SP</strong></p><p class="small"><a href="#" title="Alterar localização">Alterar localização</a></p>
-    <p>Pesquisa por palavra chave:</p>
-    <input type="text" />
-    <input type="submit" value="Pesquisar" />
+    <h1 class="title">Pesquisar Fornecedores</h1>
+    <br />
+    <p class="small">Resultados em: <strong>São Carlos/SP</strong>&nbsp;&nbsp;<span class="small">(<a href="#" title="Alterar localização">Alterar</a>)</span></p>
+    <div class="search">
+        <p>Pesquisa por palavra chave:</p>
+        <form action="fornecedores.php" method="get">
+	        <input type="text" id="keysearch" name="fornecedor" />
+		    <input type="submit" value="Pesquisar" />
+        </form>
+    </div>
     <hr />
-    <p>Pesquisa por categoria:</p>
-    <select>
-        <option value="Locais">Locais</option>
-        <option value="Bandas">Bandas</option>
-        <option value="Buffet">Buffet</option>
-        <option value="Decoração">Decoração</option>
-        <option value="Bolo &amp; Doces">Bolo &amp; Doces</option>
-        <option value="Bebidas">Bebidas</option>
-        <option value="Artigos para Festas">Artigos para Festas</option>
-        <option value="Lembranças &amp; Brindes">Lembranças &amp; Brindes</option>
-        <option value="Cozinheira">Cozinheira</option>
-        <option value="Churrasqueiro">Churrasqueiro</option>
-        <option value="Açougue">Açougue</option>
-        <option value="Serviço de Limpeza">Serviço de Limpeza</option>
-        <option value="Animadores de Festa">Animadores de Festa</option>
-        <option value="DJ">DJ</option>
-        <option value="Seguranças">Seguranças</option>        
-    </select>
-    <input type="submit" value="Pesquisar" />
+    <div class="search">
+        <p>Pesquisa por categoria:</p>
+        <form action="fornecedores.php" method="get">
+        <select name="fornecedor">
+            <option title="Locais" value="Locais" >Locais</option>
+            <option value="Bandas">Bandas</option>
+            <option value="Buffet">Buffet</option>
+            <option value="Decoração">Decoração</option>
+            <option value="Bolo &amp; Doces">Bolo &amp; Doces</option>
+            <option value="Bebidas">Bebidas</option>
+            <option value="Artigos para Festas">Artigos para Festas</option>
+            <option value="Lembranças &amp; Brindes">Lembranças &amp; Brindes</option>
+            <option value="Cozinheira">Cozinheira</option>
+            <option value="Churrasqueiro">Churrasqueiro</option>
+            <option value="Açougue">Açougue</option>
+            <option value="Serviço de Limpeza">Serviço de Limpeza</option>
+            <option value="Animadores de Festa">Animadores de Festa</option>
+            <option value="DJ">DJ</option>
+            <option value="Seguranças">Seguranças</option>        
+        </select>
+        <input type="submit" value="Pesquisar" />
+        </form>
+    </div>
 </div>
