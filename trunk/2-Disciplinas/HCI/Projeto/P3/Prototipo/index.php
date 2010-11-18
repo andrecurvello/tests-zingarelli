56017c4140383ceea74e608442b633f18318c354
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2010-11-18 20:26:17 +0000

    mudanças na home

--- trunk/2-Disciplinas/HCI/Projeto/P3/Prototipo/index.php
+++ trunk/2-Disciplinas/HCI/Projeto/P3/Prototipo/index.php
@@ -13,7 +13,19 @@
 <?php include 'header.php'; ?>
 <div id="container">
 	<?php include 'left-bar.php'; ?>
-    <div id="content">Área de conteúdo</div>
+    <div id="content">
+		<div id="index-content">
+			<h1>BEM VINDO!</h1>
+			<br /> 
+			<p>O projeto Faça Festa foi desenvolvido para te auxiliar na organização de uma festa.</p>
+			<br /> 
+			<p>Aqui você poderá consultar locais para sua festa, tipos de decoração, bandas e muito mais.</p>
+			<br /> 
+			<p>Você pode também utilizar o site para divulgar a festa pelo twitter, Orkut, Facebook e também enviar convites por e-mail para sua lista de contatos!</p>
+			<br /> 
+			<p>Tudo para fazer com que a festa para você vire sinônimo de diversão e não dor de cabeça. <strong>Aproveite!</strong></p>
+		</div>
+	</div>
 </div>
 </body>
 </html>
