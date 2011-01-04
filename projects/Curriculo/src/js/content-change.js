e071f5feb5cf895122773a1cba9356a179f3f2db
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-04 14:43:27 +0000

    Alterações finais no projeto Curriculo

--- projects/Curriculo/src/js/content-change.js
+++ projects/Curriculo/src/js/content-change.js
@@ -7,22 +7,30 @@ $(document).ready(function(){
 
 //Changes the content based on which tab user clicked
 function changeContent(number){
-	alert('here');
 	switch(number){
 		case 0: //geral selected
 			$("#text-geral").show();
 			$("#text-acad").hide();
-			$("#text-prof").hide();			
+			$("#text-prof").hide();
+			if(!$("#sidebar-geral").hasClass('selected')){$("#sidebar-geral").addClass('selected');}
+			if($("#sidebar-acad").hasClass('selected')){$("#sidebar-acad").removeClass('selected');}
+			if($("#sidebar-prof").hasClass('selected')){$("#sidebar-prof").removeClass('selected');}
 			break;
 		case 1: //academic selected
 			$("#text-geral").hide();
 			$("#text-acad").show();			
 			$("#text-prof").hide();	
+			if($("#sidebar-geral").hasClass('selected')){$("#sidebar-geral").removeClass('selected');}
+			if(!$("#sidebar-acad").hasClass('selected')){$("#sidebar-acad").addClass('selected');}
+			if($("#sidebar-prof").hasClass('selected')){$("#sidebar-prof").removeClass('selected');}
 			break;
 		case 2: //professional selected
 			$("#text-geral").hide();
 			$("#text-acad").hide();
 			$("#text-prof").show();	
+			if($("#sidebar-geral").hasClass('selected')){$("#sidebar-geral").removeClass('selected');}
+			if($("#sidebar-acad").hasClass('selected')){$("#sidebar-acad").removeClass('selected');}
+			if(!$("#sidebar-prof").hasClass('selected')){$("#sidebar-prof").addClass('selected');}
 			break;			
 	}
 }
