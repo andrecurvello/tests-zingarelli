728b4aa6de59688dbd2f51083668726e76b927d5
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-05 17:51:05 +0000

    Adição dos projetos para - SapatariaRapida: criação do novo site - carmoconfeccoes - Chacara Zingarelli

---
+++ trunk/projects/chacara/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>- Chácara Zingarelli -</title>
+<link rel="stylesheet" type="text/css" href="css/principal.css"/>
+</head>
+
+<body>
+	<?php include 'menu.php'; ?>
+    <div id="container">
+    	<div id="main-content">
+            <div id="main-title">
+            <h1>Lazer e diversão</h1>
+            <h1>para sua festa</h1>
+            </div>
+            <p>A Chácara Zingarelli é o lugar ideal para você descansar e passar um tempo livre de preocupações.</p>
+            <p>Sua infraestrutura é ótima para festas de aniversário, casamentos e comemorações.</p>
+            <p>Fique à vontade para navegar em nosso site e conhecer o que temos a lhe oferecer!</p>
+	    </div>
+        <img id="main-photo" src="img/main-bg.jpg" alt="Chácara Zingarelli - página principal" />
+        <?php include 'footer.php'; ?>
+    </div>
+    <script type="text/javascript">	
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-10206494-2']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();	
+	</script>
+</body>
+</html>
