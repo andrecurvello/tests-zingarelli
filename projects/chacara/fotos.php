17cea59bcf2b4969c126a838b828ee6e0c710e6f
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-05 17:51:05 +0000

    Adição dos projetos para - SapatariaRapida: criação do novo site - carmoconfeccoes - Chacara Zingarelli

---
+++ projects/chacara/fotos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>- Chácara Zingarelli -</title>
+<link rel="stylesheet" type="text/css" href="css/principal.css"/>
+<link rel="stylesheet" type="text/css" href="css/photoslider.css"/>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/photoslider.js"></script>
+<script type="text/javascript" src="js/functions.js"></script>
+</head>
+
+<body>
+	<?php include 'menu.php'; ?>
+    <div id="container">
+	    <!--Using photoslider-->
+		<div class="photoslider" id="default"></div>
+	   	<?php include 'footer.php'; ?>
+    </div>
+    <script type="text/javascript">	
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-10206494-2']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();	
+	</script>
+</body>
+</html>
