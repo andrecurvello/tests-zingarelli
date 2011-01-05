7a7e66ef1ae259e1c1eb70f92fbafed7f7e2bf4b
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-05 19:00:22 +0000

    Adição de SEO para chácara zingarelli

--- trunk/projects/chacara/estrutura.php
+++ trunk/projects/chacara/estrutura.php
@@ -2,8 +2,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>- Chácara Zingarelli -</title>
+<title>Chácara Zingarelli | Estrutura</title>
 <link rel="stylesheet" type="text/css" href="css/principal.css"/>
+<meta name="description" content="Estrutura da Chácara Zingarelli, descrição dos cômodos e áreas disponíveis para aluguel" />
+<meta name="keywords" content="aluguel de chácaras, salão de festas, piscina, campo de futebol, chácara araraquara" />
+<script type="text/javascript">	
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-10206494-2']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();	
+</script>
 </head>
 
 <body>
@@ -49,16 +62,5 @@
         </div>
         <?php include 'footer.php'; ?>
     </div>
-    <script type="text/javascript">	
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-10206494-2']);
-		_gaq.push(['_trackPageview']);
-		
-		(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();	
-	</script>
 </body>
 </html>
