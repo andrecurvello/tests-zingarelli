6d81246f2c80c973d47dfa59f120526df46629a2
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-08 19:42:45 +0000

    Mudança estrutura de pastas chácara

---
+++ projects/chacara/src/menu.php
@@ -0,0 +1,10 @@
+<div id="header">
+    <img src="img/topo.png" alt="Chacara Zingarelli Araraquara" usemap="#top-menu" border="0"/>
+    <map name="top-menu">
+        <area href="index.php" title="Volte para a página inicial da Chácara Zingarelli" shape="rect" coords="24,0,764,210" />
+        <area href="alugue.php" title="Saiba como alugar a Chácara Zingarelli" shape="rect" coords="60,228,160,260" />
+        <area href="estrutura.php" title="Conheça a infraestrutura da Chácara Zingarelli" shape="rect" coords="225,228,364,260" />            
+        <area href="fotos.php" title="Veja algumas fotos da Chácara Zingarelli" shape="rect" coords="428,228,507,260" />
+        <area href="localizacao.php" title="Saiba como chegar até a Chácara Zingarelli" shape="rect" coords="569,228,741,260" />
+    </map>       
+</div>
