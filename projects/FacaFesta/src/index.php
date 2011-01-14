1922a9bf1f38bea16cc55744f38a5054d9b61df0
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-14 23:33:44 +0000

    Faça a Festa - Landing Page - CSS

--- projects/FacaFesta/src/index.php
+++ projects/FacaFesta/src/index.php
@@ -4,6 +4,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.2r1/build/reset-fonts-grids/reset-fonts-grids.css" />
 <link rel="stylesheet" type="text/css" href="css/main.css"/>
+<meta name="description" content="O Faça Festa é um sistema online no auxílio à organização de pequenas festas. Aqui você encontra contato de diversos fornecedores, pode criar sua festa e divulgá-la em redes sociais." />
+<meta name="keywords" content="Faça Festa, organização de festa, fornecedores de festa, casamentos, festas infantis, churrasco, banda, divulgação de festa" />
 <title>Faça a Festa</title>
 </head>
 
@@ -11,75 +13,88 @@
 <div id="container-top"></div>
 <div id="container">
     <div id="header">
-        <img src="img/faca-festa.png" alt="Faça Festa - Logo" title="Vá página inicial do site" />
-        <div id="menu">
+        <img class="left" src="img/faca-festa.png" alt="Faça Festa - Logo" title="Vá página inicial do site" />
+        <div id="menu" class="right">
             <ul id="menu-main">
                 <li><a href="#" title="Clique aqui para se cadastrar no site" >Cadastrar</a></li>
+                <li>|</li>
                 <li><a href="#" title="Clique aqui para fazer login">Login</a></li>
             </ul>
             <form id="menu-login" action="#">
                 <label for="login">Usuário:</label>
-                <input type="text" id="login" value="login" />
+                <input type="text" id="login" name="login" size="15" />
                 <label for="password">Senha:</label>
-                <input type="password" id="password" value="password" />
-                <input type="submit" value="Ok" />
+                <input type="password" id="password" name="password" size="10" />
+                <br />
+                <input type="submit" value="Ok" style="width:60px;"/>
+                <a href="#" title="Clique aqui para cancelar o login" class="small">Cancelar</a>
             </form>
             <p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site">Usuário A</a></strong> (<a href="#" title="Clique aqui para fazer logoff" class="small">sair</a>)</p>
-            <ul id="menu-logged">
-                <li><a href="#" title="Edite as informações do seu cadastro">Minha Conta</a></li>
-                <li><a href="#" title="Veja sua lista de fornecedores favoritos">Meus Favoritos</a></li>
-                <li><a href="#" title="Veja sua lista de contatos">Meus contatos</a></li>
-                <li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
-            </ul>
-            <ul id="menu-logged-sub">
+			<ul id="menu-logged-sub" class="right">
                 <li><a href="#" title="Crie uma nova festa">Nova Festa</a></li>
                 <li><a href="#" title="Edite ou divulge uma festa criada. Gerencie a sua lista de convidados">Gerenciar Festas</a></li>       
             </ul>
-        </div>
-        <div id="search-bar">
-            <form id="search" action="#">
-                <strong><label for="search-input">Buscar</label></strong>
-                <input id="search-input" type="text" value="Digite o que está procurando" />
-                <strong><label for="search-category">em</label></strong>
-                <select id="search-category">
-                    <option title="Todas as Categorias" value="all" selected="selected">Todas as categorias</option>
-                    <option title="Faça busca por locais" value="locais">Locais</option>            
-                    <option value="Faça busca por bandas" value="bandas">Bandas</option>
-                    <option value="Faça busca por buffet" value="buffe">Buffet</option>
-                    <option value="Faça busca por decoracao" value="decoracao">Decoração</option>
-                    <option value="Faça busca por bolo &amp; Doces" value="bolos">Bolo &amp; Doces</option>
-                    <option value="Faça busca por bebidas" value="bebidas">Bebidas</option>
-                    <option value="Faça busca por artigos para Festas" value="artigos">Artigos para Festas</option>
-                    <option value="Faça busca por lembranças &amp; Brindes" value="lembrancas">Lembranças &amp; Brindes</option>
-                    <option value="Faça busca por cozinheira" value="cozinheira">Cozinheira</option>
-                    <option value="Faça busca por churrasqueiro" value="churrasqueiro">Churrasqueiro</option>
-                    <option value="Faça busca por açougue" value="acougue">Açougue</option>
-                    <option value="Faça busca por serviço de Limpeza" value="limpeza">Serviço de Limpeza</option>
-                    <option value="Faça busca por animadores de Festa" value="animadores">Animadores de Festa</option>
-                    <option value="Faça busca por DJ" value="dj">DJ</option>
-                    <option value="Faça busca por seguranças" value="seguranca">Seguranças</option>  
-                </select>
-                <input type="submit" value="Ok"/>
-            </form>
+            <ul id="menu-logged" class="right">
+                <li><a href="#" title="Edite as informações do seu cadastro">Minha Conta</a></li>
+                <li><a href="#" title="Veja sua lista de fornecedores favoritos">Meus Favoritos</a></li>
+                <li><a href="#" title="Veja sua lista de contatos">Meus Contatos</a></li>
+                <li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
+            </ul>
         </div>
     </div>
+    <div id="search-bar" class="clear">
+        <form id="search" action="#">
+            <label for="search-input">Buscar</label>
+            <input id="search-input" type="text" value="Digite o que está procurando" size="30" name="keyword"/>
+            <label for="search-category">em</label>
+            <select id="search-category" name="category">
+                <option title="Todas as Categorias" value="all" selected="selected">Todas as categorias</option>
+                <option title="Faça busca por locais" value="locais">Locais</option>            
+                <option title="Faça busca por bandas" value="bandas">Bandas</option>
+                <option title="Faça busca por buffet" value="buffe">Buffet</option>
+                <option title="Faça busca por decoracao" value="decoracao">Decoração</option>
+                <option title="Faça busca por bolo &amp; Doces" value="bolos">Bolo &amp; Doces</option>
+                <option title="Faça busca por bebidas" value="bebidas">Bebidas</option>
+                <option title="Faça busca por artigos para Festas" value="artigos">Artigos para Festas</option>
+                <option title="Faça busca por lembranças &amp; Brindes" value="lembrancas">Lembranças &amp; Brindes</option>
+                <option title="Faça busca por cozinheira" value="cozinheira">Cozinheira</option>
+                <option title="Faça busca por churrasqueiro" value="churrasqueiro">Churrasqueiro</option>
+                <option title="Faça busca por açougue" value="acougue">Açougue</option>
+                <option title="Faça busca por serviço de Limpeza" value="limpeza">Serviço de Limpeza</option>
+                <option title="Faça busca por animadores de Festa" value="animadores">Animadores de Festa</option>
+                <option title="Faça busca por DJ" value="dj">DJ</option>
+                <option title="Faça busca por seguranças" value="seguranca">Seguranças</option>  
+            </select>
+            <input type="submit" value="Ok" style="width:80px;height:31px"/>
+        </form>
+    </div>
+    <div id="content-top"></div>
     <div id="content">
-        <h1>Faça a Festa - BEM VINDO!</h1>
-        <p>O projeto Faça Festa foi desenvolvido para lhe auxiliar na organização de uma festa.</p>
-        <p>Aqui você poderá consultar locais para sua festa, tipos de decoração, bandas, buffet, fornecedores de bebidas e muito mais.</p>
-        <p>Você pode também utilizar o site para divulgar a festa pelo Twitter, Orkut, Facebook e também enviar convites por e-mail para sua lista de contatos!</p>
-        <p>Tudo para fazer com que a festa para você vire sinônimo de diversão e não dor de cabeça. Aproveite!</p>
-    </div>    
+        <div id="content-welcome">
+            <h1>Faça a Festa - BEM VINDO!</h1>
+            <p>O projeto Faça Festa foi desenvolvido para lhe auxiliar na organização de uma festa.</p>
+            <p>Aqui você poderá consultar locais para sua festa, tipos de decoração, bandas, buffet, fornecedores de bebidas e muito mais.</p>
+            <p>Você pode também utilizar o site para divulgar a festa pelo Twitter, Orkut, Facebook e também enviar convites por e-mail para sua lista de contatos!</p>
+            <p>Tudo para fazer com que a festa para você vire sinônimo de diversão e não dor de cabeça. Aproveite!</p>
+        </div>
+    </div>
+    <div id="content-bottom"</div>
 </div>
     <div id="footer">
         <ul>
             <li><a href="#" title="Informações sobre a segurança e utilização dos seus dados">Política de Privacidade</a></li>
+            <li>|</li>
             <li><a href="#" title="Informações sobre as responsabilidades garantidas pelo site">Termos de Uso</a></li>
+            <li>|</li>
             <li><a href="#" title="Entenda como o site funciona e o que está disponível para seu uso">Ajuda</a></li>
+            <li>|</li>
             <li><a href="#" title="Entre em contato conosco para fazer críticas/elogios e dar sugestões">Contato</a></li>
+            <li>|</li>
             <li><a href="#" title="Veja todas as seções disponíveis no site">Mapa do site</a></li>
-            <li><a href="#" title="Área de acesso reservado para o cadastro de fornecedores">Área para fornecedores</a></li>
+            <li>|</li>
+            <li><strong><a href="#" title="Área de acesso reservado para o cadastro de fornecedores">Área para fornecedores</a></strong></li>
         </ul>
+        <p class="very-small">Sistema desenvolvido por: <a href="http://www.icmc.usp.br/~zinga" title="Vá para a página do desenvolvedor do sistema">Matheus Zingarelli</a>
     </div>
 </body>
 </html>
