81c10743803868b6f0f629b5e0de89f62bc5e781
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-01-24 00:03:20 +0000

    Faça Festa: Atualização do backlog e adição da pasta tutoriais

---
+++ projects/FacaFesta/tutoriais/ajax-login-form/do_login.php
@@ -0,0 +1,32 @@
+<?php
+if(empty($_POST)) exit;
+
+include 'config.php';
+
+// declare post fields
+
+$post_username = trim($_POST['username']);
+$post_password = trim($_POST['password']);
+
+$post_autologin = $_POST['autologin'];
+
+if(($post_username == $config_username) && ($post_password == $config_password))
+{
+$_SESSION['username'] = $config_username;
+
+// Autologin Requested?
+
+if($post_autologin == 1)
+	{
+	$password_hash = md5($config_password); // will result in a 32 characters hash
+
+	setcookie ($cookie_name, 'usr='.$config_username.'&hash='.$password_hash, time() + $cookie_time);
+	}
+
+exit('OK');
+}
+else
+{
+echo '<div id="error_notification">The submitted login info is incorrect.</div>';
+}
+?>
---
+++ projects/FacaFesta/tutoriais/ajax-login-form/readme.txt
@@ -0,0 +1,9 @@
+| __ )(_) |_    |  _ \ ___ _ __   ___  ___(_) |_ ___  _ __ _   _ 
+|  _ \| | __|   | |_) / _ \ '_ \ / _ \/ __| | __/ _ \| '__| | | |
+| |_) | | |_    |  _ <  __/ |_) | (_) \__ \ | || (_) | |  | |_| |
+|____/|_|\__|   |_| \_\___| .__/ \___/|___/_|\__\___/|_|   \__, |
+                        |_|                              |___/ 
+
+This archive is downloaded from http://www.bitrepository.com/. Visit us for more snippets & tutorials.
+
+Tutorial URL: http://www.bitrepository.com/web-programming/ajax/login-form.html
