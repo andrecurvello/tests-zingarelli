2d563aacd587edef24610f35cb1db853918d1ca2
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-02-04 14:57:45 +0000

    Faca Festa - Login - Mudanças para funcionar o Jquery+AJAX

--- trunk/projects/FacaFesta/src/do_login.php
+++ trunk/projects/FacaFesta/src/do_login.php
@@ -1,12 +1,12 @@
 <?php
 include 'config.php';
 
-//if(empty($_POST)){header("location:index.php");exit;}
+if(empty($_POST)){header("location:index.php");exit;}
 
 //get data
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
-$fromJavascript = trim($_POST['fromJavascript]']);
+$fromJavascript = trim($_POST['fromJavascript']);
 
 //Prevent SQL Injection
 $username = mysql_escape_string($username);
@@ -16,45 +16,46 @@ $fromJavascript = mysql_escape_string($fromJavascript);
 //Encrypts password
 $password = md5($password);
 
-//TODO:change to db connection
 //connect to MySQL
-$db = mysql_connect(DB_SERVER, DB_USER, DB_PASS) or die('Falha na conexao. Verifique as credenciais.' . mysql_error());
+$db = mysql_connect(DB_SERVER, DB_USER, DB_PASS,false, 65536) or die('Falha na conexao. Verifique as credenciais.' . mysql_error());
 mysql_select_db(DB_NAME, $db) or die ('Nao foi possivel conectar no banco' . DB_NAME);
 
 //call stored procedure to check login
-$query = sprintf("CALL verifyLogin(`%s`, `%s`, @nome)", $username, $password);
-
-$result = mysql_query($query, $db);
+$query = sprintf("CALL verifyLogin('%s', '%s', @nome);", $username, $password);
+$result = mysql_query($query);
 $result = mysql_query("SELECT @nome");
 
-if($result){ //login verified
-
-	while($row = mysql_fetch_array($result)){
-		$_SESSION['logged'] = '1';
-		echo $row['nome'];
-		if($fromJavascript == 'yes'){
-			//print the welcome page
-			echo '
-				<p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site">Usuário A</a></strong> (<a href="logout.php" title="Clique aqui para fazer logoff" class="small">sair</a>)</p>
-				<ul id="menu-logged-sub" class="right">
-					<li><a href="#" title="Crie uma nova festa">Nova Festa</a></li>
-					<li><a href="#" title="Edite ou divulge uma festa criada. Gerencie a sua lista de convidados">Gerenciar Festas</a></li>       
-				</ul>
-				<ul id="menu-logged-main" class="right">
-					<li><a href="#" title="Edite as informações do seu cadastro">Minha Conta</a></li>
-					<li><a href="#" title="Veja sua lista de fornecedores favoritos">Meus Favoritos</a></li>
-					<li><a href="#" title="Veja sua lista de contatos">Meus Contatos</a></li>
-					<li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
-				</ul>';
-		}
-		else{
-			//user has come from a normal HTTP Post request, redirect to main page
-			header("location:index.php");
-		}
+$row = mysql_fetch_array($result);
+if(!empty($row['@nome'])){ //login verified	
+	$_SESSION['logged'] = '1';
+	if($fromJavascript == 'yes'){
+		//print the welcome page
+		echo '
+			<p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site">'.$row['@nome'].'</a></strong> (<a href="logout.php" title="Clique aqui para fazer logoff" class="small">sair</a>)</p>
+			<ul id="menu-logged-sub" class="right">
+				<li><a href="#" title="Crie uma nova festa">Nova Festa</a></li>
+				<li><a href="#" title="Edite ou divulge uma festa criada. Gerencie a sua lista de convidados">Gerenciar Festas</a></li>       
+			</ul>
+			<ul id="menu-logged-main" class="right">
+				<li><a href="#" title="Edite as informações do seu cadastro">Minha Conta</a></li>
+				<li><a href="#" title="Veja sua lista de fornecedores favoritos">Meus Favoritos</a></li>
+				<li><a href="#" title="Veja sua lista de contatos">Meus Contatos</a></li>
+				<li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
+			</ul>';
+	}
+	else{
+		//user has come from a normal HTTP Post request, redirect to main page
+		header("location:index.php");
 	}
 }
-else
+else //wrong login information
 {
-	die("Erro: " . mysql_error());
+	$_SESSION['login-error'] = 1;
+	if($fromJavascript == 'yes'){
+		echo "N";
+	}
+	else{
+		header("location:index.php");
+	}
 }
 ?>
