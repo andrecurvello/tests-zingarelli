542c666c52e0c53d93a19055e6a112f7d872a0da
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-02-07 15:06:32 +0000

    Faca Festa - Ultimos acertos na pagina de login e verificação nos navegadores

--- trunk/projects/FacaFesta/src/do_login.php
+++ trunk/projects/FacaFesta/src/do_login.php
@@ -28,19 +28,17 @@ $result = mysql_query("SELECT @nome");
 $row = mysql_fetch_array($result);
 if(!empty($row['@nome'])){ //login verified	
 	$_SESSION['logged'] = '1';
+	$_SESSION['fullname'] = $row['@nome'];
 	if($fromJavascript == 'yes'){
 		//print the welcome page
 		echo '
-			<p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site">'.$row['@nome'].'</a></strong> (<a href="logout.php" title="Clique aqui para fazer logoff" class="small">sair</a>)</p>
-			<ul id="menu-logged-sub" class="right">
-				<li><a href="#" title="Crie uma nova festa">Nova Festa</a></li>
-				<li><a href="#" title="Edite ou divulge uma festa criada. Gerencie a sua lista de convidados">Gerenciar Festas</a></li>       
-			</ul>
-			<ul id="menu-logged-main" class="right">
+			<p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site">'.$_SESSION['fullname'].'</a></strong>!</p>
+			<ul id="menu-logged" class="left">
 				<li><a href="#" title="Edite as informações do seu cadastro">Minha Conta</a></li>
 				<li><a href="#" title="Veja sua lista de fornecedores favoritos">Meus Favoritos</a></li>
 				<li><a href="#" title="Veja sua lista de contatos">Meus Contatos</a></li>
 				<li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
+				<li><a href="logout.php" title="Clique aqui para fazer logoff">Sair</a></li>
 			</ul>';
 	}
 	else{
--- trunk/projects/FacaFesta/src/header.php
+++ trunk/projects/FacaFesta/src/header.php
@@ -6,7 +6,7 @@
 	<div id="container-top"></div>
 	<div id="container">
         <div id="header">
-            <a href="index.php" title="Vá para a página inicial do site"><img class="left" src="img/faca-festa.png" alt="Faça Festa - Logo" title="Página inicial do site" /></a>
+            <a href="index.php" title="Vá para a página inicial do site"><img id="logo" class="left" src="img/faca-festa.png" alt="Faça Festa - Logo" title="Página inicial do site" /></a>
             <div id="menu" class="right">
                 <?php if ($_SESSION['logged']==0) {//not logged ?>
                     <ul id="menu-main">
@@ -34,16 +34,13 @@
                 <?php 
 					} else{ ?>
                     	<!--WARNING: any changes to this content has to be changed in do_login.php file as well-->
-						<p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site">Usuário A</a></strong> (<a href="logout.php" title="Clique aqui para fazer logoff" class="small">sair</a>)</p>
-						<ul id="menu-logged-sub" class="right">
-							<li><a href="#" title="Crie uma nova festa">Nova Festa</a></li>
-							<li><a href="#" title="Edite ou divulge uma festa criada. Gerencie a sua lista de convidados">Gerenciar Festas</a></li>       
-						</ul>
-						<ul id="menu-logged-main" class="right">
+						<p>Olá, <strong><a href="#" title="Clique para ver seu perfil no site"><?php echo $_SESSION['fullname'];?></a></strong>!</p>
+						<ul id="menu-logged" class="left">	                        
 							<li><a href="#" title="Edite as informações do seu cadastro">Minha Conta</a></li>
 							<li><a href="#" title="Veja sua lista de fornecedores favoritos">Meus Favoritos</a></li>
 							<li><a href="#" title="Veja sua lista de contatos">Meus Contatos</a></li>
-							<li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
+                            <li><a href="#" title="Crie uma nova festa ou gerencie festas já existentes">Minhas Festas</a></li>
+                            <li><a href="logout.php" title="Clique aqui para fazer logoff">Sair</a></li>				
 						</ul>
                 <?php }	?>
 				<div id="menu-logged"></div><!--menu-logged (for javascript validation)-->
