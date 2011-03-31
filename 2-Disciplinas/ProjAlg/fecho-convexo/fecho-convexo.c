d9eda4e40d39220ad235930c2666d2780e469944
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-03-31 01:59:20 +0000

    ProjAlg - finalização do fecho convexo - adição do caixeiro viajante (em andamento)

--- 2-Disciplinas/ProjAlg/fecho-convexo/fecho-convexo.c
+++ 2-Disciplinas/ProjAlg/fecho-convexo/fecho-convexo.c
@@ -57,13 +57,11 @@ int main(int argc, char* argv[]){
     fp = fopen(argv[1], "r");
 
 //debug
-    if(!fp) {printf("\nErro na abertura do arquivo"); return -1;}
-    
-    //lê número de pontos
+    /*if(!fp) {printf("\nErro na abertura do arquivo"); return -1;}*/
+       
+    //lê pontos e armazena em um vetor de pontos
     int n = 0;
     fscanf(fp, "%d", &n);
-    
-    //lê pontos e armazena em um vetor de pontos
     struct par_ordenado vet_pontos[n];
     for(i = 0; i < n; i++){
        fscanf(fp, "%d %d", &vet_pontos[i].pX, &vet_pontos[i].pY);
@@ -71,15 +69,7 @@ int main(int argc, char* argv[]){
     
     fclose(fp);
     
-//debug
-    /*for(i = 0; i < n; i++){
-          printf("%d %d\n", vet_pontos[i].pX, vet_pontos[i].pY);
-    }
-    printf("%d\n\n", n);*/
-    
     //cálculo do fecho convexo através da busca das arestas que o formam
-    struct par_ordenado vet_resultado[n]; //irá armazenar as arestas do fecho convexo
-    l = 0; //índice para armazenar pontos no vetor de resultado
     for(i = 0; i <= n-2; i++){
           p1 = vet_pontos[i];
           for(j = i+1; j <= n-1; j++){
@@ -99,26 +89,21 @@ int main(int argc, char* argv[]){
                            if(result > c) lado1++;
                            else if(result < c) lado2++;
                            //se result = c o ponto está sobreposto no segmento de reta
-                           
-                           //printf("Busca X: %d Busca Y: %d\n a: %d, b: %d, c: %d, result: %d lado1: %d, lado2: %d\n", pBusca.pX, pBusca.pY,a, b, c, result, lado1, lado2);
-                           //printf("p1X %d p1Y %d\nP2X %d p2Y %d\nbuscaX %d buscaY %d\n\n", p1.pX, p1.pY, p2.pX, p2.pY,pBusca.pX, pBusca.pY);
                       }
                 }
                 /* Para o segmento ser considerado aresta do fecho convexo, 
                 todos os pontos devem estar ou em lado1 ou em lado2. Se estiverem
                 em ambos os lados o segmento não é aresta do fecho convexo.
                 Se ambos os lados forem 0, todos os pontos estão alinhados e são 
-                arestas do fecho convexo.                                
-                */
+                arestas do fecho convexo. */
                 if(lado1 == 0 || lado2== 0){
-                   vet_resultado[l++]=p1;
-                   vet_resultado[l++]=p2;
+                   printf("%d %d\n", p1.pX, p1.pY);
+                   printf("%d %d\n", p2.pX, p2.pY);
                 }
+                /* Melhoria que poderia ser feita: verificar se uma aresta não
+                está contida em uma aresta maior. Mesmo assim, o algoritmo funciona
+                de maneira correta */
           }
-    }
-    
-    for(i = 0; i < l; i++)
-          printf("%d %d\n", vet_resultado[i].pX, vet_resultado[i].pY);
-    
+    }        
     return 0;
 }
