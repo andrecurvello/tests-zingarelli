f94b4f56ec5fceedd67fb1fa70461923d04151e7
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-04-16 23:00:21 +0000

    ProjAlg - código final do gerador de permutações utilizando o algoritmo de johnson-trotter.

--- trunk/2-Disciplinas/ProjAlg/gerar-permutacoes/johnson-trotter.c
+++ trunk/2-Disciplinas/ProjAlg/gerar-permutacoes/johnson-trotter.c
@@ -28,11 +28,16 @@
 --------------------*/
 #include <stdio.h>
 
-/* Estrutura para armazenar um dígito móvel, com seu valor e o valor do dígito
-para o qual ele aponta */
+/* Estrutura para armazenar um dígito móvel 
+   valor: valor do dígito
+   apontaParaIdx: índice para qual elemento da lista ele aponta, -1 caso ele seja
+                  um extremo da lista apontando para posição fora da lista
+   direcao: 0 se ele aponta para elemento à esquerda dele, 1 para a direita
+*/
 typedef struct digito{
         int valor;
-        int aponta_para;
+        int apontaParaIdx;
+        int direcao;
 } Digito;
 
 /* Imprime os valores contidos na lista de dígitos (valor de uma permutação) 
@@ -41,7 +46,7 @@ typedef struct digito{
    Saída: valor dos dígitos na linha de comando */
 void imprime(Digito *vet, int n){
     int i;
-    for(i = 0; i < n; i++){
+    for(i = 1; i < n+1; i++){
         printf("%d ", vet[i].valor);
     }
     printf("\n");
@@ -54,42 +59,92 @@ maior deles
    Saída: índice do maior dígito móvel da lista. -1 se não houver dígito móvel */
 int MaiorDigitoMovel(Digito *vet, int n){
     int i;
-    int maior = -1;
-    for(i = 0; i < n; i++){
-        if(vet[i].aponta_para < vet[i].valor){ //é dígito móvel
-            if(maior < vet[i].valor){
-                maior = i;
+    int maiorValor = 0;
+    int indice = -1;
+    for(i = 1; i < n+1; i++){
+        /* verifica se o valor contido no índice para o qual o dígito aponta é
+        menor do que o valor desse */
+        if(vet[vet[i].apontaParaIdx].valor < vet[i].valor){ //é dígito móvel
+            if(maiorValor < vet[i].valor){
+                maiorValor = vet[i].valor;
+                indice = i;
             }
         }
     }
-    return maior;
+    return indice;
 }
 
 int main(int argc, char* argv[]){
     int n, i, k;
     scanf("%d",&n);
-    Digito vet[n];
+    Digito vet[n], aux;
     
-    /* Construção da lista de dígitos */
-    vet[0].valor = 1;
-    vet[0].aponta_para = 0;
-    for(i = 1; i < n; i++){
-        vet[i].valor = i+1;
-        vet[i].aponta_para = i;
+    /* Construção da lista de dígitos com dois sentinelas nas extremidades 
+     para evitar que os dígitos nas posições 1 e n sejam móveis quando apontando
+     para estas extremidades */
+    vet[0].valor = 100000;
+    vet[0].apontaParaIdx = -1;
+    vet[0].direcao = 0;
+    vet[n+1].valor = 100000;
+    vet[n+1].apontaParaIdx = -1;
+    vet[n+1].direcao = 0;
+    for(i = 1; i < n+1; i++){
+        vet[i].valor = i;
+        vet[i].apontaParaIdx = i-1;
+        vet[i].direcao = 0;
     }
     
     imprime(vet, n);
     
     k = MaiorDigitoMovel(vet, n);
     
-    while(k != -1){
+    while(k != -1){//enquanto houver dígito móvel
+        //troca dígito móvel em k com o dígito para o qual ele aponta
+        aux.valor = vet[k].valor;
+        aux.apontaParaIdx = vet[k].apontaParaIdx;
+        aux.direcao = vet[k].direcao;
+        vet[k].valor = vet[aux.apontaParaIdx].valor;
+        vet[k].direcao = vet[aux.apontaParaIdx].direcao;
+        vet[k].apontaParaIdx = k-1;
+        switch(vet[k].direcao){
+            case 0: //aponta para elemento à esquerda
+                 vet[k].apontaParaIdx = k-1;
+                 break;
+            case 1: //aponta para elemento à direita
+                 vet[k].apontaParaIdx = k+1;
+                 break;                    
+        }
+        vet[aux.apontaParaIdx].valor = aux.valor;
+        vet[aux.apontaParaIdx].direcao = aux.direcao;
+        switch(vet[aux.apontaParaIdx].direcao){
+            case 0: //aponta para elemento à esquerda
+                 vet[aux.apontaParaIdx].apontaParaIdx = aux.apontaParaIdx-1;
+                 break;
+            case 1: //aponta para elemento à direita
+                 vet[aux.apontaParaIdx].apontaParaIdx = aux.apontaParaIdx+1;
+                 break;                    
+        }
         
+        //inverte direção do apontaPara dos elementos maiores que dígito móvel em k
+        for(i = 1; i < n+1; i++){
+            if(vet[i].valor > aux.valor){
+                //verifica a direcao e inverte
+                switch(vet[i].direcao){
+                    case 0: //aponta para elemento à esquerda
+                         vet[i].direcao = 1;
+                         vet[i].apontaParaIdx = i+1;
+                         break;
+                    case 1: //aponta para elemento à direita
+                         vet[i].direcao = 0;
+                         vet[i].apontaParaIdx = i-1;
+                         break;
+                }
+            }
+        }
+        
+        imprime(vet, n);
+        k = MaiorDigitoMovel(vet, n);
     }
-    
-    //debug
-    /*for(i = 0; i < n; i++){
-        printf("Valor %d aponta para %d\n", vet[i].valor, vet[i].aponta_para);
-    }*/
     
     return 0;
 }
