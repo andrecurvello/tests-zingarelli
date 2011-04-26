db6b23d2dadb76d5b80c5a40493985452446b063
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-04-26 21:58:34 +0000

    Hipermidia - atualização do código do projeto, criação do botão que converte pra xml

--- 2-Disciplinas/Hipermidia/Projeto/NCLCreator/js/tools.js
+++ 2-Disciplinas/Hipermidia/Projeto/NCLCreator/js/tools.js
@@ -28,4 +28,33 @@ $(function(){
 			$(".ui-selectee").draggable('destroy').resizable('destroy');
 		}
 	});
+	
+	$("#getNCL").click(function(){
+		//get width, height, top and left position of each region
+		$(".box").each(function(index){
+			var style = $(this).attr("style").split(';');
+			for(var i = 0; i < style.length;i++){
+				var attribute = style[i].split(':');
+				switch('left'){//TODO MAKE THIS SWITCH WORK!
+				case 'left':
+					document.write('left'+attribute[1]+'<br />');
+					break;
+				case 'top':
+					document.write('top'+attribute[1]+'<br />');
+					break;
+				case 'width':
+					document.write('width'+attribute[1]+'<br />');
+					break;
+				case 'height':
+					document.write('height'+attribute[1]+'<br />');
+					break;
+				default:
+					document.write('None found<br />');
+				}
+				
+			}
+		});
+		
+		return false;
+	});
 });
