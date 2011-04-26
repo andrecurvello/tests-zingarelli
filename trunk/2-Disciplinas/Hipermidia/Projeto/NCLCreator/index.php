8bc691f47c0237128c2806d6c3574d1cde654ed9
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-04-26 19:28:49 +0000

    Hipermídia - criação da pasta contendo o projeto

---
+++ trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Testing draggable and resizable</title>
+<script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.11.custom.min.js"></script>
+<script type="text/javascript" src="js/test.js"></script>
+<link type="text/css" href="css/jquery-ui-1.8.11.custom.css" rel="stylesheet" />	
+<link type="text/css" href="css/main-drag.css" rel="stylesheet" />
+</head>
+<body>
+	<div id="desktop">
+		<div id="selectable">
+		</div>		
+	</div>
+	<div id="toolbar">
+		<a href="#" id="createRegion">Criar região</a><br />
+		<a href="#" id="createTextBox">Criar caixa de texto</a><br />
+		<a href="#" id="createVideo">Adicionar vídeo</a><br />
+		<a href="#" id="createSound">Adicionar som</a><br />
+	</div>
+</body>
+</html>
