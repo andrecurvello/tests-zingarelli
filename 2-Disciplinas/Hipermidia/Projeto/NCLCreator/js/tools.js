c4944a98bd4e5485a15e8118e10df4467966a0da
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-05-02 17:42:18 +0000

    Hipermidia - corrigido o javascript para pegar os dados de estilo dos elementos.

--- 2-Disciplinas/Hipermidia/Projeto/NCLCreator/js/tools.js
+++ 2-Disciplinas/Hipermidia/Projeto/NCLCreator/js/tools.js
@@ -1,10 +1,12 @@
 $(function(){
+	idCounter = 0;
+	
 	$("#desktop").click(function(){
 		$(".ui-selected").removeClass("ui-selected").draggable('destroy').resizable('destroy');
 	});
 	
 	$("#createRegion").click(function(){
-		$("#selectable").append("<div class='box'>Drag and Resize me!</div>")
+		$("#selectable").append("<div class='box' id='box-"+ idCounter++ +"'>Drag and Resize me!</div>")
 	});
 	
 	$("#selectable").selectable({
@@ -32,29 +34,27 @@ $(function(){
 	$("#getNCL").click(function(){
 		//get width, height, top and left position of each region
 		$(".box").each(function(index){
-			var style = $(this).attr("style").split(';');
-			for(var i = 0; i < style.length;i++){
-				var attribute = style[i].split(':');
-				switch('left'){//TODO MAKE THIS SWITCH WORK!
-				case 'left':
-					document.write('left'+attribute[1]+'<br />');
-					break;
-				case 'top':
-					document.write('top'+attribute[1]+'<br />');
-					break;
-				case 'width':
-					document.write('width'+attribute[1]+'<br />');
-					break;
-				case 'height':
-					document.write('height'+attribute[1]+'<br />');
-					break;
-				default:
-					document.write('None found<br />');
+			style = $(this).attr("style").split(' ');
+			regId = $(this).attr("id");
+			document.write('ID: ' + regId + '<br />');
+			for(i = 0; i < style.length; i++){//for each attribute, store the ones we're interested in
+				switch(style[i]){
+					case "left:":
+						document.write('left: '+style[i+1]+'<br />');
+						break;
+					case 'top:':
+						document.write('top: '+style[i+1]+'<br />');
+						break;
+					case 'width:':
+						document.write('width: '+style[i+1]+'<br />');
+						break;
+					case 'height:':
+						document.write('height: '+style[i+1]+'<br />');
+						break;
 				}
-				
 			}
+			//TODO:construid JSON com os dados e enviar para o PHP
 		});
-		
 		return false;
 	});
 });
