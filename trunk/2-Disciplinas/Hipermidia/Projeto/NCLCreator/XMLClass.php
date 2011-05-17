423b8d21628ba1fea94ee81b70ad99571cfc19a2
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-05-17 23:14:39 +0000

    Hipermídia - funcionalidade para adicionar imagens

--- trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/XMLClass.php
+++ trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/XMLClass.php
@@ -3,6 +3,9 @@ class NCLDocument {
 	private $regionBase;
 	private $port;
 	private $media;
+	//Parte 2....
+//	private $link;
+//	private $bind;
 		
 	public function addRegion($id,$left,$top,$width,$height,$descriptorName) {
 		$this->regionBase[] = array(
@@ -19,7 +22,7 @@ class NCLDocument {
 		$this->port[] = array(
 			'id'		=>	$id,
 			'component'	=>	$component
-			);
+		);
 	}
 	
 	public function addMedia($id,$type,$src,$descriptor) {
@@ -30,14 +33,31 @@ class NCLDocument {
 			'descriptor'	=>	$descriptor
 			);
 	}
+	//Parte 2...
+	/*
+	public function addLink($id,$xconnector) {
+		$this->link[] = array (
+			'id'		=> 	$id,
+			'xconnector'	=>	$xconnector
+		);	
+	}
+	
+	public function addBind($linkid,$component,$role) {
+		$this->bind[] = array (
+			'linkid' 	=> 	$linkid,
+			'component'	=>	$component,
+			'role'		=> 	$role		
+		);	
+	}*/
 
 	public function createXML($appName) {
 		$doc = new DOMDocument('1.0','ISO-8859-1');
 		$doc->formatOutput = true;
 		
-		/*set file to be downloaded by user*/
-		/*header('Content-type: "text/xml"; charset="ISO-8859-1"');
-    	header('Content-disposition: attachment; filename="example.xml"');*/
+		/*set file to be downloaded by user
+		header('Content-type: "text/xml"; charset="ISO-8859-1"');
+    		header('Content-disposition: attachment; filename="example.xml"');
+    		*/
 		
 		$ncl = $doc->createElement("ncl");
 		$ncl->setAttribute("id",$appName);
@@ -52,7 +72,7 @@ class NCLDocument {
 				
 		$rb = $doc->createElement("regionBase");
 		$rb = $head->appendChild($rb);	
-				
+		
 		foreach($this->regionBase as $reg) {					
 			$region = $doc->createElement("region");
 			$region->setAttribute("id",$reg['id']);
@@ -73,7 +93,43 @@ class NCLDocument {
 			$desc = $descriptorbase->appendChild($desc);
 		}
 		
-		/*foreach($this->port as $reg) {
+		
+		//Comentado == Parte 2 do projeto...
+		/*
+		$connector_base = $doc->createElement("connectorBase");
+		$connector_base = $head->appendChild($connector_base);
+		
+		foreach($this->connectorBase as $con) {
+			$connector = $doc->createElement("causalConnector");
+			$connector->setAttribute('id',$con['id']);
+			$connector = $connector_base->appendChild($connector);
+			
+			foreach($this->connectorParam as $con_par) {
+				if($con_par['connectorid'] == $con['id']) {
+					$aux = $doc->createElement("connectorParam");
+					$aux->setAttribute("name",$con_par['name']);
+					$aux = $connector->appendChild($aux);
+				}			
+			}
+			
+			foreach($this->simpleCondition as $simc) {
+				if($simc['connectorid'] == $con['id']) {
+					$aux = $doc->createElement("simpleCondition");
+					$aux->setAttribute("role",$simc['role']);
+					$aux->setAttribute("key",$simc['key']);
+					$aux = $connector->appendChild($aux);
+				}			
+			}
+			
+			//Incompleto....
+					
+		}
+		*/
+		
+		
+		
+		/*
+		foreach($this->port as $reg) {
 			$port = $doc->createElement("port");
 			$port->setAttribute('id',$reg['id']);
 			$port->setAttribute('component',$reg['component']);
@@ -87,7 +143,28 @@ class NCLDocument {
 			$media->setAttribute("src",$reg['src']);
 			$media->setAttribute("descriptor",$reg['descriptor']);
 			$media = $body->appendChild($media);
-		}		*/
+		}	
+		*/
+		
+		
+		//Cria os links e os binds....
+		//Parte 2 do projeto...
+		/*
+		foreach($this->link as $alink) {
+			$link = $doc->createElement("link");
+			$link->setAttribute('id',$alink['id']);
+			$link->setAttribute('xconnector',$alink['xconnector']);
+			$link = $body->appendChild($link);	
+			foreach($this->bind as $abind) {
+				if($abind['linkid'] == $alink['id']) {
+					$bind = $doc->createElement("bind");
+					$bind->setAttribute('component',$abind['component']);
+					$bind->setAttribute('role',$abind['role']);
+					$bind = $link->appendChild($bind);
+				}
+			}	
+		}	*/
+		
 		
 		$doc->save("teste.xml");
 	}
