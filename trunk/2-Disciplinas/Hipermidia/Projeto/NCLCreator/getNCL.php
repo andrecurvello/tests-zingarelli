9cbea8771c0c76d5021ce7e017dcf74be9cde376
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-05-19 01:29:32 +0000

    hipermidia -alteraçoes layout e javascript

--- trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/getNCL.php
+++ trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/getNCL.php
@@ -1,14 +1,25 @@
 <?php
-require_once 'XMLClass.php';
-$nclDoc = new NCLDocument();
-
-foreach($_POST["array"] as $arr) {
-	if($arr[0] == 'region') {
-		$nclDoc->addRegion($arr[1],$arr[2],$arr[3],$arr[4],$arr[5],$arr[6]);
-	} else {
-		//Caso não seja uma região....
+	require_once 'XMLClass.php';
+	$nclDoc = new NCLDocument();
+	$counter = 0;
+	foreach($_POST["array"] as $arr) {
+		switch($arr[0]) {
+			case 'region':
+				$nclDoc->addRegion($arr[1],$arr[2],$arr[3],$arr[4],$arr[5],$arr[6]);
+				break;			
+			case 'media':
+				$nclDoc->addRegion($arr[1],$arr[2],$arr[3],$arr[4],$arr[5],$arr[6]);
+				$nclDoc->addMedia($arr[7], $arr[8],$arr[9],$arr[6]);
+				$nclDoc->addPort("Port-".$counter,$arr[7]);
+				break;		
+			default:
+				break;
+		}
+		$counter++;
 	}
-}
-$nclDoc->createXML('teste');
-echo 'true';
+	
+	// media + idRegion + left + top + width + height + descriptorName + idMedia + imgType + src
+			
+	$nclDoc->createXML('teste');
+	echo 'true';
 ?>
