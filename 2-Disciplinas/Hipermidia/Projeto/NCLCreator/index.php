ee6c5de95fb4e94348927f7c4f64a1a98887c57a
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-05-19 05:18:13 +0000

    Hipermídia - primeira versão final

--- 2-Disciplinas/Hipermidia/Projeto/NCLCreator/index.php
+++ 2-Disciplinas/Hipermidia/Projeto/NCLCreator/index.php
@@ -4,27 +4,26 @@
 <script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.11.custom.min.js"></script>
 <script type="text/javascript" src="js/tools.js"></script>
-<script type="text/javascript" src="js/json2.js"></script>
+<script type="text/javascript" src="js/jquery.fileupload-ui.js"></script>
+<script type="text/javascript" src="js/jquery.fileupload.js"></script>
 <link type="text/css" href="css/jquery-ui-1.8.11.custom.css" rel="stylesheet" />	
 <link type="text/css" href="css/main-drag.css" rel="stylesheet" />
 </head>
 <body>
-	<div id="desktop">
+	<h1>NCL Creator</h1>
+    <div id="desktop">
 		<!-- Elementos sem background -->
 		<div id="selectable">
 		</div>		
 		<!-- Elementos de mídia (imagens, vídeo, flash) -->
-		<div id="image"></div>
+		<div id="media"></div>
 	</div>
 	<div id="toolbar">
 		<ul id="menu" class="left">
 			<li><a href="#" id="createRegion" title="Adicionar região">Adicionar região</a></li>
-			<li><a href="#" id="createTextBox" title="Adicionar caixa de texto">Adicionar caixa de texto</a></li>
 			<li><a href="#" id="createVideo" title="Adicionar vídeo">Adicionar vídeo</a></li>
-			<li><a href="#" id="createSound" title="Adicionar som">Adicionar som</a></li>
-			<li>
-				<a href="#" id="createImage" title="Adicionar imagem">Adicionar imagem</a>
-			</li>		
+			<li><a href="#" id="createFlash" title="Adicionar flash">Adicionar flash</a></li>
+			<li><a href="#" id="createImage" title="Adicionar imagem">Adicionar imagem</a></li>		
 			<li>
 				<form action="getNCL.php" id="getNCL" method="post">
 					<input id="btngetNCL" type="submit" title="Gerar NCL" value="Gerar NCL" />
@@ -32,16 +31,12 @@
 			</li>
 		</ul>
 		<div id="sub-menu" class="left">
-			<div id="text-menu" >
-				<textarea rows="6" cols="20"></textarea>
-				<input type="button" id="btnText" value="Ok" />
-			</div>
-			<div id="image-menu" >
-				<span>Localização da imagem: </span>
-				<input type="file" id="imagePath"/>
-				<input type="submit" id="btnImage" value="Ok" />
-				<span><a href="#" title="Cancelar" id="cancelImage">Cancelar</a></span>
-			</div>
+            <form id="media-menu" method="post">
+                <span>Localização da mídia: </span>
+                <input type="file" id="mediaPath" name="mediaPath"/>
+                <input type="submit" id="btnMedia" value="Ok" />
+                <span><a href="#" title="Cancelar" id="cancelMedia">Cancelar</a></span>
+            </form>
 		</div>
 	</div>
 </body>
