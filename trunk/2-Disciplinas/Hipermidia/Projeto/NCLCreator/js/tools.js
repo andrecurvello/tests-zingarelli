a5390c1db4bcba3865afc76afe5172d2690955f4
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2011-05-19 05:18:13 +0000

    Hipermídia - primeira versão final

--- trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/js/tools.js
+++ trunk/2-Disciplinas/Hipermidia/Projeto/NCLCreator/js/tools.js
@@ -1,6 +1,7 @@
 $(function(){
-	$("#image-menu").hide();
-	$("#text-menu").hide();
+	$("#media-menu").hide();
+	/*$("#flash-menu").hide();
+	$("#image-menu").hide();*/
 	
 	idCounter = 0;
 	
@@ -9,28 +10,54 @@ $(function(){
 	});
 	
 	$("#createRegion").click(function(){
-		$("#selectable").append("<div class='box' id='box-"+ idCounter++ +"'>Drag and Resize me!</div>")
+		$("#selectable").append("<div class='box' style='position:absolute;top:0;left:0' id='box-"+ idCounter++ +"'>Região</div>")
 	});
 	
-	/*Tratamendo para adição de imagem*/
-	$("#createImage").click(function(){
-		$("#image-menu").show('fast');
+	/*Tratamendo para mídias*/
+	//RESTRIÇÃO: mídias devem se encontrar na mesma pasta que o código! TODO: fazer upload da mídia
+	//imagem
+	tipoMidia = 'none';
+	$("#createVideo").click(function(){
+		$("#media-menu").show('fast');
+		tipoMidia = 'video';
 	});
-	$("#cancelImage").click(function(){$("#image-menu").hide('fast');});
-	$("#btnImage").click(function(){alert("oi");
-		$("#image").append("<div style='width:1px;' class='image-wrapper'><img id='image-"+ idCounter++ +"' class='image' src='"+$("#imagePath").val()+"'></div>");
-		$("#image div").draggable({
+	$("#createFlash").click(function(){
+		$("#media-menu").show('fast');
+		tipoMidia = 'flash';
+	});
+	$("#createImage").click(function(){
+		$("#media-menu").show('fast');
+		tipoMidia = 'image';
+	});	
+	$("#cancelMedia").click(function(){$("#media-menu").hide('fast');});
+	$("#btnMedia").click(function(){
+		switch(tipoMidia){
+			case 'video':
+				$("#media").append("<div style='position:absolute;top:0;left:0;width:1px;' class='video-wrapper'><img width=100 height=100 id='video-"+ idCounter++ +"' class='video' src='img/video.jpg'></div>");
+				srcVideo = $("#mediaPath").val();
+				break;
+			case 'flash':
+				$("#media").append("<div style='position:absolute;top:0;left:0;width:1px;' class='flash-wrapper'><img width=100 height=100 id='flash-"+ idCounter++ +"' class='flash' src='img/flash.jpg'></div>");
+				srcFlash = $("#mediaPath").val();
+				break;
+			case 'image':
+				$("#media").append("<div style='position:absolute;top:0;left:0;width:1px;' class='image-wrapper'><img width=100 height=100 id='image-"+ idCounter++ +"' class='image' src='"+$("#mediaPath").val()+"'></div>");
+				break;
+		}
+		$("#media div").draggable({
 							containment:'#desktop', //define os limites de onde a região pode ser arrastada
 							stack:'#desktop div', //define o z-index da imagem, que regiões ficarão abaixo dela
 							opacity:0.40,
 							grid: [10, 10]}); //nível de opacidade para quando se está arrastando a imagem
-		$("#image img").resizable({grid: [10, 10]});
+		$("#media img").resizable({grid: [10, 10]});
 		//infelizmente, a imagem consegue ultrapassar a área delimitada para se trabalhar, pois quem é draggable é a div.
 		//não é possível tornar a imagem draggable e resizable. Por isso é criado um div.
 		//tutorial tirado em http://bugs.jqueryui.com/ticket/3446	
+		$("#media-menu").hide();
+		return false;
 	});
 	
-	/*Tratamento para outros (que nome dar??)*/
+	/*Tratamento para region)*/
 	$("#selectable").selectable({
 		selecting:function(event, ui){
 			$(".ui-selecting")
@@ -65,14 +92,40 @@ $(function(){
 		$(".image").each(function(index){
 			arr[control] = ['media',
 			                "region-"+$(this).attr("id"),
-			                $(this).parent().css("left"),
-			                $(this).parent().css("top"), 
+			                $(this).parent().parent().css("left"),
+			                $(this).parent().parent().css("top"), 
 			                $(this).css("width"), 
 			                $(this).css("height"), 
 			                "descriptor-" + $(this).attr("id"), 
 			                $(this).attr("id"), 
 			                "image/jpeg",
 			                $(this).attr("src")];
+			control++;
+		});
+		$(".video").each(function(index){
+			arr[control] = ['media',
+			                "region-"+$(this).attr("id"),
+			                $(this).parent().parent().css("left"),
+			                $(this).parent().parent().css("top"), 
+			                $(this).css("width"), 
+			                $(this).css("height"), 
+			                "descriptor-" + $(this).attr("id"), 
+			                $(this).attr("id"), 
+			                "image/jpeg", //colocar descritor de video
+			                srcVideo];
+			control++;
+		});
+		$(".flash").each(function(index){
+			arr[control] = ['media',
+			                "region-"+$(this).attr("id"),
+			                $(this).parent().parent().css("left"),
+			                $(this).parent().parent().css("top"), 
+			                $(this).css("width"), 
+			                $(this).css("height"), 
+			                "descriptor-" + $(this).attr("id"), 
+			                $(this).attr("id"), 
+			                "image/jpeg", //colocar descritor de flash
+			                srcFlash];
 			control++;
 		});
 		//send post request 
@@ -82,7 +135,7 @@ $(function(){
 					$("#sub-menu").append("<a target='_blank' href='teste.xml'>Download do arquivo</a>");				
 				}
 				else{
-					document.write('nada');					
+					alert('error');					
 				}
 			});
 		return false;
