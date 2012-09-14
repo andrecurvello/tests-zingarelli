ca3e5c31e0dd6d340897fc8f1c9100a89f843d00
thesolitaryreader@gmail.com <thesolitaryreader@gmail.com@4b75f230-aabb-2bc2-6d83-520395076aa2>
2012-09-14 13:37:53 +0000

    1) Dando uma olhada no código, eu percebi que eram calculadas as mudanças de posição de cada pixel tanto nas coordenadas horizontais (x) quando nas verticais (y). Como no par estéreo o deslocamento ocorre apenas horizontalmente (as imagens testadas já estão retificadas), comentei a parte do código que atualizava a coordenada y do pixel reconstruído. Essa mudança gerou uma pequena melhoria na imagem (os detalhes ficaram mais nítidos, diminuiu a presença de riscos horizontais).
    2) Fiz pequenas adições ao código para aproveitar o mesmo fluxo e distâncias calculados e reconstruir R2 e B2 correspondente à imagem do lado direito. Mesmo neste caso tendo que reconstruir dois canais, eu considerei que o resultado ficou superior ao da reconstrução da imagem esquerda.
    
    3) Criei um anáglifo verde-magenta com estas duas imagens reconstruídas e, como era de se esperar, o resultado ficou ótimo, com percepção de profundidade (já que os canais de cores envolvidos na construção do anáglifo verde magenta - R1 G2 B1 - não foram mexidos). 
    
    4) Criei então um anáglifo vermelho-ciano (R2 G1 B1), que utiliza dois canais reconstruídos, e a imagem resultante perdeu toda a informação de profundidade.

--- trunk/4-OpenCV/optical-flow/opflowrest.cpp
+++ trunk/4-OpenCV/optical-flow/opflowrest.cpp
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
       int i, option = 0;
 
       Mat im1, im2;
-      
-      im1 = imread(argv[1]);
+
+      im1 = imread(argv[1]); //left image
       if (!im1.data) {
 	  printf("Nao foi possivel carregar imagem de entrada 1\n");
 	  return -1;
       }
-      im2 = imread(argv[2]);
+      im2 = imread(argv[2]); //right image
       if (!im1.data) {
 	  printf("Nao foi possivel carregar imagem de entrada 2\n");
 	  return -1;
       }
-      
+
       // create vector of 3 images to split channels
       // true images
       vector<Mat> RGBtrue1;
@@ -45,28 +45,28 @@ int main(int argc, char *argv[])
 
       // images to estimate channels
       vector<Mat> RGBest1;
-      RGBest1.push_back( RGBtrue1.at(0)); // B1
-      RGBest1.push_back( Mat::zeros(im1.rows, im1.cols, CV_8UC1)); // G (empty)
-      RGBest1.push_back( RGBtrue1.at(2)); // R1
-      
+      RGBest1.push_back(RGBtrue1.at(0)); // B1
+      RGBest1.push_back(Mat::zeros(im1.rows, im1.cols, CV_8UC1)); // G1 (empty)
+      RGBest1.push_back(RGBtrue1.at(2)); // R1
+
       Mat imRB, grayRB1, grayG2;
       merge(RGBest1, imRB);
       cvtColor(imRB, grayRB1, CV_RGB2GRAY); // gray image composed by R1 and B1 channels
       grayG2 = Mat(RGBtrue2.at(1));
-      
+
       // image just to compare
       vector<Mat> RGBest2;
-      RGBest2.push_back( RGBtrue1.at(0)); // B1
-      RGBest2.push_back( RGBtrue2.at(1)); // G2
-      RGBest2.push_back( RGBtrue1.at(2)); // R1
+      RGBest2.push_back(Mat::zeros(im1.rows, im1.cols, CV_8UC1)); // B2 (empty)
+      RGBest2.push_back(RGBtrue2.at(1)); // G2
+      RGBest2.push_back(Mat::zeros(im1.rows, im1.cols, CV_8UC1)); // R2 (empty)
       // flow matrix (velocity in x and y directions)
       Mat flow;
 
-      // blur before computing optical flow 
+      // blur before computing optical flow
       medianBlur(grayRB1, grayRB1, 3);
       medianBlur(grayG2, grayG2, 3);
-      
-      // InputArray prev, InputArray next, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma, int flags 
+
+      // InputArray prev, InputArray next, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma, int flags
       // 		 prev            next               flow    pscale  lev wins  it   polyn polys flags
       // 		const cv::Mat&, const cv::Mat&,   cv::Mat&, double, int, int, int, int, double, int
       //calcOpticalFlowFarneback(RGBtrue2.at(1), RGBest1.at(2), flow, 0.5,     1,    1,  10, 2 , 1.5,  0 );
@@ -77,29 +77,34 @@ int main(int argc, char *argv[])
       split(flow, xy);
 
       int y, x;
-      
-      // compute the displacement of each pixel from G_2 to R_1
+
+      // compute the displacement of each pixel from G2 to RB1
       for( x = 0; x < flow.rows; x++)  {
 	  for( y = 0; y < flow.cols; y++) {
 	      //grab flow for X and Y
 	      float vx = xy[0].at<float>(x,y);
-	      float vy = xy[1].at<float>(x,y);
-	      
+	      //float vy = xy[1].at<float>(x,y);
+
 	      //calculate displacement
 	      int dx = (int)x+vx;
-	      int dy = (int)y+vy;
+	      //int dy = (int)y+vy;
 	      // check for out of bounds values
 	      dx = (dx > flow.rows || dx < 0) ? 0 : dx;
-	      dy = (dy > flow.cols || dy < 0) ? 0 : dy;
-	      
-	      // copy value from G_2 to G_1
-	      RGBest1.at(1).at<uchar>(x,y) = RGBtrue2.at(1).at<uchar>(dx,dy);
+	      //dy = (dy > flow.cols || dy < 0) ? 0 : dy;
+
+	      // copy value from G2 to G1
+	      RGBest1.at(1).at<uchar>(x,y) = RGBtrue2.at(1).at<uchar>(dx,y);
+	      // copy value from R1 and B1 to R2 and B2
+	      RGBest2.at(0).at<uchar>(x,y) = RGBtrue1.at(0).at<uchar>(dx,y);
+          RGBest2.at(2).at<uchar>(x,y) = RGBtrue1.at(2).at<uchar>(dx,y);
 	  }
       }
-      
+
       // median blur to remove small noise
       medianBlur(RGBest1.at(1), RGBest1.at(1), 3);
-      
+      medianBlur(RGBest2.at(0), RGBest2.at(0), 3);
+      medianBlur(RGBest2.at(2), RGBest2.at(2), 3);
+
       // merge the channels to form the final restored image
 
       // image with new estimated G channel
@@ -113,19 +118,54 @@ int main(int argc, char *argv[])
 //       //calculate angle and magnitude
 //       Mat magnitude, angle;
 //       cartToPolar(xy[0], xy[1], magnitude, angle, true);
-// 
+//
 //       //translate magnitude to range [0;1]
 //       double mag_max;
 //       minMaxLoc(magnitude, NULL, &mag_max, NULL, NULL, Mat());
 //       magnitude.convertTo(magnitude, -1, 1.0/mag_max);
 
       // show restored image
-      namedWindow("G recovery", CV_WINDOW_AUTOSIZE );
-      imshow("G recovery", im1est);
-      
+//      namedWindow("G recovery", CV_WINDOW_AUTOSIZE );
+//      imshow("G recovery", im1est);
+
       // write image in the disk
-      imwrite("out_recover.png", im1est);
-      imwrite("out_displaced.png", im2est);
+      imwrite("out_recover_left.png", im1est);
+      imwrite("out_recover_right.png", im2est);
+
+
+      //create green-magenta anaglyph with the recovered images
+      vector<Mat> gmrec;
+      gmrec.push_back(RGBest1.at(0)); // B1
+      gmrec.push_back(RGBest2.at(1)); // G2
+      gmrec.push_back(RGBest1.at(2)); // R1
+
+      Mat im3gm;
+      merge(gmrec, im3gm);
+
+      imwrite("out_green_magenta_recover.png",im3gm);
+
+      //create red-cyan anaglyph with the recovered images
+      vector<Mat> rcrec;
+      rcrec.push_back(RGBest1.at(0)); // B1
+      rcrec.push_back(RGBest1.at(1)); // G1
+      rcrec.push_back(RGBest2.at(2)); // R2
+
+      Mat im4rc;
+      merge(rcrec, im4rc);
+
+      imwrite("out_red_cyan_recover.png",im4rc);
+
+      //true red-cyan anaglyph
+      vector<Mat> rctrue;
+      rctrue.push_back(RGBtrue1.at(0)); // B1
+      rctrue.push_back(RGBtrue1.at(1)); // G1
+      rctrue.push_back(RGBtrue2.at(2)); // R2
+
+      Mat im5rc;
+      merge(rctrue, im5rc);
+
+      imwrite("out_red_cyan_true.png",im5rc);
+
       waitKey(0);
       return 0;
 }
